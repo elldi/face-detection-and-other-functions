dde3b57be1c66cccaea36137adfbc0b02f01479b
Adam7238 <adam.ward5@btinternet.com>
2018-03-10 20:06:21 +0000

    just prints out how many faces found in the picture
    Had a nightmare getting it to work at home so this is all I've managed so far.

--- start.py
+++ start.py
@@ -14,7 +14,18 @@ cascadePath = sys.argv[2]
 #------------------------------------------------------------------------------
 # Functions
 #------------------------------------------------------------------------------
-
+def Faces():
+  image = cv2.imread(imageScan)
+  grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  
+  faceFinder = cv2.CascadeClassifier(cascadePath)
+  
+  cv2.imshow("Original" ,image)
+  cv2.imshow("Grey" ,grey)
+  
+  facesFound = faceFinder.detectMultiScale(grey,scaleFactor = 1.2,minNeighbours=10,minSize=(30,30),flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+  
+  print("Found {} faces!".format(len(facesFound)))
 #------------------------------------------------------------------------------
 # Main
 #------------------------------------------------------------------------------
