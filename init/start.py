800ca5dd6c0de67258ee71cbcb8bd2461cdcbf91
Adam7238 <adam.ward5@btinternet.com>
2018-04-10 12:01:58 +0100

    Remove duplicated functions from start. Import lib.py instead

--- init/start.py
+++ init/start.py
@@ -4,67 +4,8 @@ import cv2
 import numpy as np
 import sys
 import argparse
+import lib
 
-
-#------------------------------------------------------------------------------
-# Global Variables
-#------------------------------------------------------------------------------
-
-
-#------------------------------------------------------------------------------
-# Functions
-#------------------------------------------------------------------------------
-def faces(imageScan, cascadePath, toCut):
-
-  image = cv2.imread(imageScan)
-  if(image is None):
-    print("Image not found!")
-    return 0
-
-  grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-  faceFinder = cv2.CascadeClassifier(cascadePath)
-
-  # cv2.imshow("Original" ,image)
-  # cv2.imshow("Grey" ,grey)
-  
-  facesFound = faceFinder.detectMultiScale(grey,1.3,5)  
-  print("Found {} faces!".format(len(facesFound)))
-
-  for (x,y,w,h)in facesFound:
-    cv2.rectangle(image, (x,y),(x+w, y+h), (0,255,0), 2)
-
-    if(toCut):
-      cutImage = image[y:y+h, x: x+w]
-      cv2.imshow("Faces found",cutImage)
-      cv2.waitKey(0)
-    
-  cv2.imshow("Faces found",image)
-  cv2.waitKey(0)
-
-def videoCapture(xml, camera):
-
-	video = cv2.VideoCapture(camera)
-	cascade = cv2.CascadeClassifier(xml)
-
-	while(video.isOpened()):
-		returnValue, frame = video.read()
-
-		cv2.imshow("Original", frame)
-		
-		facesFound = cascade.detectMultiScale(frame,1.3,5)  
-
-  		print("Found {} faces!".format(len(facesFound)))
-
-  		for (x,y,w,h) in facesFound:
-    			cv2.rectangle(image, (x,y),(x+w, y+h), (0,255,0), 2)
-
-		cv2.imshow("Face Find", frame)
-
-		if key == 27: # exit on ESC
-        		break
-	video.release()
-	cv2.destroyAllWindows()
 #------------------------------------------------------------------------------
 # Main
 #------------------------------------------------------------------------------
